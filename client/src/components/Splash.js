a5bbbf364fdf4181ee22198bd3e17b7d2a0ffc83
dustinpezley <dustinpezley@gmail.com>
2022-10-08 12:10:09 -0500

    dashboard component nearly complete

--- client/src/components/Splash.js
+++ client/src/components/Splash.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
@@ -5,12 +6,25 @@ import MainLogo from '../assets/images/APPly.png';
 import './Splash.css';
 
 const Splash = () => {
+  const { isLoginModalOpen, setLoginModalOpen } = useState(false);
+  const { isSignupModalOpen, setSignupModalOpen } = useState(false);
+
   return (
     <Container fluid id="splash-container">
       <img id="splash-logo" src={MainLogo} alt="APPly logo" />
       <ButtonGroup id="splash-btn-group" aria-label="login and signup">
-        <Button id="login-btn">Login</Button>
-        <Button id="signup-btn">Signup</Button>
+        <Button
+          id="login-btn"
+          onClick={() => setLoginModalOpen(!isLoginModalOpen)}
+        >
+          Login
+        </Button>
+        <Button
+          id="signup-btn"
+          onClick={() => setSignupModalOpen(!isSignupModalOpen)}
+        >
+          Signup
+        </Button>
       </ButtonGroup>
     </Container>
   );
