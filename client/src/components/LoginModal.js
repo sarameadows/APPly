3e67e80357218df18a9c23418e6314d29e9ed322
Louie Moretti <morettilouie@gmail.com>
2022-10-08 04:19:24 -0500

    captured inputs

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginModal.css'
 
 function Login() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, password } = formState;
+    
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        console.log(formState);
+    }
+
     return (
         <div id="login" class="login">
             <div class="login-box">
                 <h3>Login!</h3>
-                <form class="login-form">
-                    <label for="username">Username:</label>
-                    <input type="text"></input>
-                    <br></br>
-                    <label for="password">Password:</label>
-                    <input type="text"></input>
+                <form class="login-form" onSubmit={handleSubmit}>
+                    <div>
+                        <label for="username">Username:</label>
+                        <input type="text" defaultValue={formState.username} onChange={handleChange} name="username" />
+                    </div>
+                    <div>
+                        <label for="password">Password:</label>
+                        <input type="text" defaultValue={formState.password} onChange={handleChange} name="password" />
+                    </div>
+                    <button type='submit'>Submit</button>
                 </form>
             </div>
         </div>
--- client/src/components/SignupModal.js
+++ client/src/components/SignupModal.js
@@ -2,22 +2,40 @@ import React from 'react';
 import './LoginModal.css'
 
 function SignUp() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { username, password, email, github } = formState;
+    
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        console.log(formState);
+    }
     return (
         <div id="signup" class="signup">
             <div class="signup-box">
                 <h3>Sign up!</h3>
-                <form class="signup-form">
-                    <label for="username">Username:</label>
-                    <input type="text"></input>
-                    <br></br>
-                    <label for="password">Password:</label>
-                    <input type="text"></input>
-                    <br></br>
-                    <label for="email">Email:</label>
-                    <input type="text"></input>
-                    <br></br>
-                    <label for="github">GitHub:</label>
-                    <input type="text"></input>
+                <form class="signup-form" onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" defaultValue={formState.username} onChange={handleChange} name='username' />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password:</label>
+                        <input type="text" defaultValue={formState.password} onChange={handleChange} name='password' />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email address:</label>
+                        <input type="email" defaultValue={formState.email} onChange={handleChange} name="email" />
+                    </div>
+                    <div>
+                        <label htmlFor="github">GitHub:</label>
+                        <input type="text" defaultValue={formState.github} onChange={handleChange} name='github' />
+                    </div>
+                    <button type='submit'>Submit</button>
                 </form>
             </div>
         </div>
