938034a637768fcefd09fdebc8cdf3c90a96629b
Sara <sameads9@gmail.com>
2022-10-10 14:47:54 -0500

    auth.js and API.js

---
+++ client/src/utils/API.js
@@ -0,0 +1,42 @@
+// route to get logged in user's info (needs the token)
+export const getMe = (token) => {
+    return fetch('/api/users/me', {
+        headers: {
+            'Content-Type': 'application/json',
+            authorization: `Bearer ${token}`,
+        },
+    });
+};
+
+// route to create a user
+export const createUser = (userData) => {
+    return fetch('/api/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+    });
+};
+  
+export const loginUser = (userData) => {
+    return fetch('/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+    });
+};
+  
+// save job data for a logged in user
+export const addJob = (jobData, token) => {
+    return fetch('/api/users', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(jobData),
+    });
+};
