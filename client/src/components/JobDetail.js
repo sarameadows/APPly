84e9b048bed1a1a85df3b3b73e3c6db8b5277697
dustinpezley <dustinpezley@gmail.com>
2022-10-10 15:11:18 -0500

    started job detail modal

--- client/src/components/JobDetail.js
+++ client/src/components/JobDetail.js
@@ -1,6 +1,30 @@
-const JobDetail = ({onClose, currentJob}) => {
-  const { dateApplied, datePosted, title, company, link, location, officeSetting, pay, source, applicationStatus, requirements, benefits } = currentJob;
-  return(
-    
-  )
-}
+import Modal from 'react-bootstrap/Modal';
+
+const JobDetail = ({ currentJob }) => {
+  const {
+    dateApplied,
+    datePosted,
+    title,
+    company,
+    link,
+    location,
+    officeSetting,
+    pay,
+    source,
+    applicationStatus,
+    requirements,
+    benefits,
+  } = currentJob;
+  return (
+    <Modal size="lg" aria-labelledby="contained-modal-title-center" centered>
+      <Modal.Header closeButton>
+        <Modal.Title id="detail-title">{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h1 id="detail-company">{company}</h1>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default JobDetail;
